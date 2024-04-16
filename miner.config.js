d198848f1941b061fac9330320ffa4562863bf54
Rahul Arulkumaran <rarulkumaran@foundrydigital.com>
2024-04-16 09:37:53 -0500

    update miner config file to take hf_repo_id

--- miner.config.js
+++ miner.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'miner',
       script: 'python3',
-      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network local --wallet.name walletName --wallet.hotkey hotkeyName --axon.port 8091 --model base_lstm.h5'
+      args: './neurons/miner.py --netuid 28 --logging.debug --logging.trace --subtensor.network local --wallet.name walletName --wallet.hotkey hotkeyName --axon.port 8091 --hf_repo_id foundryservices/bittensor-sn28-base-lstm --model mining_models/base_lstm_new.h5'
     },
   ],
 };
