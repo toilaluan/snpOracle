b947a8f29f93478bbb5d11830da2f8bfb5aad868
Rahul Arulkumaran <rarulkumaran@foundrydigital.com>
2024-05-20 17:49:02 -0500

    add logic to init wandb

--- neurons/validator.py
+++ neurons/validator.py
@@ -17,9 +17,13 @@
 
 import time
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 import pandas_market_calendars as mcal
 import yfinance as yf
 import pytz
+import pathlib
+import wandb
+import os
 
 # Bittensor
 import bittensor as bt
@@ -30,7 +34,9 @@ from predictionnet.validator import forward
 
 # import base validator class which takes care of most of the boilerplate
 from predictionnet.base.validator import BaseValidatorNeuron
+from predictionnet import __version__
 
+load_dotenv()
 
 class Validator(BaseValidatorNeuron):
     """
@@ -48,6 +54,28 @@ class Validator(BaseValidatorNeuron):
         self.load_state()
 
         # TODO(developer): Anything specific to your use case you can do here
+        netrc_path = pathlib.Path.home() / ".netrc"
+        wandb_api_key = os.getenv("WANDB_API_KEY")
+        if wandb_api_key is not None:
+            bt.logging.info("WANDB_API_KEY is set")
+        bt.logging.info("~/.netrc exists:", netrc_path.exists())
+
+        if wandb_api_key is None and not netrc_path.exists():
+            bt.logging.warning(
+                "WANDB_API_KEY not found in environment variables."
+            )
+        
+        wandb.init(
+                project=f"sn{self.config.netuid}-validators",
+                entity="foundryservices",
+                config={
+                    "hotkey": self.wallet.hotkey.ss58_address,
+                },
+                name=f"validator-{self.uid}-{__version__}",
+                resume="auto",
+                dir=self.config.neuron.full_path,
+                reinit=True,
+        )
         
     
     async def is_market_open(self, time):
