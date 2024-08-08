d1a8cb786399d8477155f0d45fde0c546c7c4f4d
hscott <hscott@foundrydigital.com>
2024-08-08 13:14:00 -0400

    added print statements to debug

--- predictionnet/base/neuron.py
+++ predictionnet/base/neuron.py
@@ -86,6 +86,7 @@ class BaseNeuron(ABC):
         bt.logging.info(f"Subtensor: {self.subtensor}")
 
         # The metagraph holds the state of the network, letting us know about other validators and miners.
+        print(self.config)
         self.metagraph = self.subtensor.metagraph(self.config.netuid)
         bt.logging.info(f"Metagraph: {self.metagraph}")
 
